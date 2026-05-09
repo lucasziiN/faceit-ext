13a9403cfb27a7f30ac737ce98c1cd633f2137c5
lucasziiN <lucassoda2@gmail.com>
2026-05-09 13:08:55 +1200

    Retrieve first (own) player data

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,1 +1,22 @@
-console.log("hello world")
+declare const process: { env: { FACEIT_API_KEY: string } }
+console.log("service worker started")
+async function getData() {
+    try {
+        const FACEIT_API_KEY = process.env.FACEIT_API_KEY
+        const response = await fetch('https://open.faceit.com/data/v4/players?nickname=lucasziiN', {
+            headers: {
+                'Authorization': `Bearer ${FACEIT_API_KEY}`
+            }
+        })
+
+        // Check if response was successful
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.error('fetch error: ', error)
+    }
+}
+getData()
