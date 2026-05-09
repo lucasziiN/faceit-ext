3760536565050be51de792fcee4ebac65b980fdd
lucasziiN <lucassoda2@gmail.com>
2026-05-09 19:19:06 +1200

    Add functionality for retrieving data for all players in a match

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,4 +1,4 @@
-import { getMatchDetails } from './faceit-api'
+import { getMatchDetails, getPlayerStatsById} from './faceit-api'
 
 function handleMessages(message, sender, sendresponse) {
     if (message.type === 'MATCH_ROOM_DETECTED') {
@@ -12,7 +12,17 @@ chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
     if (details.url.includes('/cs2/room/')) {
         const matchId = details.url.split('/').pop()
         if (matchId){
-            getMatchDetails(matchId).then(data => console.log(data))
+            handleMatchRoom(matchId)
         }
     }
 });
+
+async function handleMatchRoom(matchId: string){
+    const matchDetails = await getMatchDetails(matchId)
+    const team1 = matchDetails.teams.faction1.roster
+    const team2 = matchDetails.teams.faction2.roster
+    const allPlayers = [...team1, ...team2]
+    
+    const results = await Promise.all(allPlayers.map(player => getPlayerStatsById(player.player_id)))
+    console.log(results)
+}
